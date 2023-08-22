dd0eb4236916b574b5899400d29d02cc44bb5994
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-08-22 11:08:33 +0300

    media

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Profile
+
+
+def render_photo(obj):
+    if obj.photo:
+        img_html = '<img src="{url}" width="{width}" height="{height}" />' \
+            .format(url=obj.photo.url,
+                    width=64,
+                    height=64
+                    )
+        return mark_safe(img_html)
+    return None
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [render_photo, 'user', 'date_of_birth']
+    raw_id_fields = ['user']
