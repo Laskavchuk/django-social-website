f27be7d2faf26e368ae39e81eb40eb0d560fa9bc
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-10-25 14:44:30 +0300

    list_image

--- images/urls.py
+++ images/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from images.views import image_create, image_detail, image_like
+from images.views import image_create, image_detail, image_like, image_list
 
 app_name = 'images'
 
 urlpatterns = [
     path('create/', image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
-    path('like/', image_like, name='like')
+    path('like/', image_like, name='like'),
+    path('', image_list, name='list'),
 ]
--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 
@@ -73,3 +74,27 @@ def image_like(request):
         return redirect(request.META.get('HTTP_REFERER'))
     except Image.DoesNotExist:
         return JsonResponse({'status': 'error'})
+
+
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html', {
+            'section': 'images',
+            'images': images}
+                      )
+    return render(request, 'images/image/list.html', {
+            'section': 'images',
+            'images': images}
+                      )
